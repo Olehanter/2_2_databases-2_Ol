e375d9132488f9cb5cac802eb997989e8528f0a1
Olehanter <Olehanter@gmail.com>
2023-06-04 21:17:36 +0300

    Change Admin

--- m2m-relations/articles/admin.py
+++ m2m-relations/articles/admin.py
@@ -1,8 +1,35 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
+from .models import Article, Scope, Tag
 
-from .models import Article
+
+class RelationshipInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        count = 0
+        for form in self.forms:
+            if 'is_main' in form.cleaned_data:
+                if form.cleaned_data['is_main']:
+                    count += 1
+            if count == 0:
+                raise ValidationError('Определите основной раздел')
+            elif count > 1:
+                raise ValidationError('Основной только один раздел')
+
+            return super().clean()
+
+
+class RelationshipInline(admin.TabularInline):
+    model = Scope
+    formset = RelationshipInlineFormset
 
 
 @admin.register(Article)
-class ArticleAdmin(admin.ModelAdmin):
-    pass
+class ObjectAdmin(admin.ModelAdmin):
+    inlines = [RelationshipInline]
+
+
+@admin.register(Tag)
+class ObjectAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    list_filter = ['name']
